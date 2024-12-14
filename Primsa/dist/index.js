d10ed804bded9da21f80cbd457a9b924fd0f5d68
Sudarsan Mansingh <sudarsan2k5@gmail.com>
2024-12-14 16:17:02 +0530

    added user

--- Primsa/dist/index.js
+++ Primsa/dist/index.js
@@ -1,2 +1,26 @@
 "use strict";
-console.log("Hi");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client");
+const client = new client_1.PrismaClient();
+function createUser() {
+    return __awaiter(this, void 0, void 0, function* () {
+        client.user.create({
+            data: {
+                username: "Sudarsan",
+                password: "123sd",
+                age: 23,
+                city: "Delhi"
+            }
+        });
+    });
+}
+createUser();
--- Primsa/src/index.ts
+++ Primsa/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hi")
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+
+async function createUser(){
+    client.user.create({
+        data: {
+            username: "Sudarsan",
+            password: "123sd",
+            age: 23,
+            city: "Delhi"
+        }
+    })
+}
+
+createUser();
